503e1ee29b4b6252975a6e7db84a7b6ab787cb7f
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-07-18 22:08:29 -0300

    Name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar=()=>{
     return (
 <div className="container">
-   <h1 className="w-full font-bold text-2xl"> Welcome to my Todo List app </h1>
+   <h1 className="w-full font-bold text-2xl"> Welcome to Listify </h1>
 </div>
     )
 }
